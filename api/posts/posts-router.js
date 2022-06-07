e0aa2364db293d30e16a376038dfc35b1020157c
renee.troiani@gmail.com <renee.troiani@gmail.com>
2022-06-07 13:59:21 -0600

    find post by id complete

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -10,7 +10,23 @@ router.get('/', (req, res) => {
         res.status(200).json(result);
     })
     .catch(error => {
-        res.status(500).json({message: 'something went wrong retrieving the posts :('})
+        console.log(error)
+        res.status(500)
+        .json({message: 'something went wrong retrieving the posts :('})
+    })
+})
+
+router.get('/:id', (req, res) => {
+    Post.findById(req.params.id).then(result => {
+        if(result == null){
+            res.status(404).json({ message: 'does not exist' });
+            return;
+        }
+        res.status(200).json(result);
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({ message: 'something went wrong :('})
     })
 })
 
